9fc4eec28407e941546142a827a32c7b80ac7c15
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-09-08 18:01:08 -0500

    This function retrieves an element from a list like in C.

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    i = 0
     for run in my_list:
+        i = i + 1
         if idx < 0:
             return (None)
         if run == idx:
             return run + 1
-    if idx > run - 1:
+    if idx > i - 1:
         return (None)
