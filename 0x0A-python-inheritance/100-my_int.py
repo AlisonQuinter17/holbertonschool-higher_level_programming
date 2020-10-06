f716ce56cb190b4fd622b01e5b030d91e49113dd
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-10-06 18:08:03 -0500

    MyInt has == and != operators inverted.

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Module for MyInt subclass."""
+'''Module for MyInt class.'''
 
 
 class MyInt(int):
     def __eq__(self, other):
+        """If equals, inverting it."""
         return int(self) != int(other)
 
     def __ne__(self, other):
+        """If not-equals, inverting it."""
         return int(self) == int(other)
