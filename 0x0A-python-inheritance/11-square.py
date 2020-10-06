ea9f956937d8aa88be0fe2ffa26fe0ece79e2ea9
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-10-06 18:32:12 -0500

    This subclass Square print the area of the Square (Str).

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,14 +7,14 @@ class Square(Rectangle):
     """The Square subclass."""
     def __init__(self, size):
         """Instantiation with size."""
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
         """This method returns the area of the square."""
-        return (self.__size * self.__size)
+        return self.__size * self.__size
 
     def __str__(self):
         """This method represent the Square str values."""
-        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
