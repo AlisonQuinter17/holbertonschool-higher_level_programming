a305f66fef70863eb13cd59661821a1c25c0168f
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-10-07 18:01:13 -0500

    This function reads n lines of a text file.

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module for read_lines function."""
+
+
+def read_lines(filename="", nb_lines=0):
+    """This function reads n lines of a text file."""
+    with open(filename, "r", encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.readlines(), end="")
+
+        else:
+            x = 0
+            for i in f:
+                x = x + 1
+                if x <= nb_lines:
+                    print(i, end="")
