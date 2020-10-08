ecddf4e9f9e53f556e02af489702f7cf7b26c1fa
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-10-07 23:51:43 -0500

    Pascal triangle.

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Method for pascal_triangle function."""
+
+
+def pascal_triangle(n):
+    """The Pascal Triangle function."""
+    if row <= 0:
+        return []
+    elif row == 1:
+        return [[1]]
+    result = [[1]]
+    while len(result) != row:
+        last_row = result[-1]
+        new_row = [1]
+        for i in range(len(last_row) - 1):
+            new_row.append(last_row[i] + last_row[i + 1])
+        new_row += [1]
+        result.append(new_row)
+
+    return result
