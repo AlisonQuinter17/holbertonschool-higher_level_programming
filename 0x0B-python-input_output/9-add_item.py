da27ec1e7465013cf6c74d9f55a28ccd7da793b6
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-10-07 21:04:22 -0500

    This function adds all arguments to a Python list, and then save them to a file.

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,8 +9,9 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 try:
     container = load_from_json_file('add_item.json')
-    for i in range(1, len(sys.argv)):
-        container.append(sys.argv[i])
-    save_to_json_file(container, 'add_item.json')
 except:
-    save_to_json_file([], 'add_item.json')
+    container = []
+
+for i in range(1, len(sys.argv)):
+    container.append(sys.argv[i])
+save_to_json_file(container, 'add_item.json')
