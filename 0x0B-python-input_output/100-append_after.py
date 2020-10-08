86a3fc702bfab2d11d91def124dbcb12a905eb3e
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-10-08 00:33:09 -0500

    This function search and insert a string on a text file.

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Module for append_filename function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """This function search and inserts a line of text to a file."""
+    with open(filename, "r", encoding="utf-8") as f:
+        container = f.readlines()
+
+    with open(filename, "w", encoding="utf-8") as f:
+        for x in container:
+            f.write(x)
+            if x.count(search_string) != 0:
+                f.write(new_string)
