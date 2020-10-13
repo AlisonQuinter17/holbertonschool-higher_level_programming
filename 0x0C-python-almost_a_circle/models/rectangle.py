9c05976368243787ce94630cfa5cb848bc49183f
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-10-13 12:21:46 -0500

    This Rectangle Subclass.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -46,7 +46,7 @@ class Rectangle(Base):
     def height(self, value):
         """Setter - Define the height value of the rectangle."""
         if type(value) != int:
-            raise TypeError("heigth must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
