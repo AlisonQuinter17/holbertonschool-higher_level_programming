fb08aa6bb4c149506d086a1b61bf4f0baea23883
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-11-17 17:49:29 -0500

    Update

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,4 @@
 -- lists all the cities of California that can be found in the database.
--- The states table contains only one record where name = California (but the id can be different, as per the example)
--- Results must be sorted in ascending order by cities.id
--- You are not allowed to use the JOIN keyword
--- The database name will be passed as an argument of the mysql command
+-- WHERE -  relational database management system.
 SELECT id, name FROM cities;
-WHERE state_id = (SELECT id FROM states WHERE name='California')ORDER BY id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name="California");
