c99f7800362c2b3b497de4c50ec8e432f3179c8a
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-12-11 23:49:13 -0500

    lists all records with a score >= 10 in the table of the database.

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10 in the table of the database
-SELECT score, name FROM second_table WHERE score='14' OR score='10'
+SELECT score, name FROM second_table WHERE score>='10' ORDER BY score DESC;
