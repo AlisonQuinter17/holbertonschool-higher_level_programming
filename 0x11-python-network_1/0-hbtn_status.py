e3453725a03832b7575ccc6618cac041dfe22ddb
Alison Quintero <alisondanellaquintero@hotmail.com>
2021-01-11 13:30:56 -0500

    What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for fetch a URL."""
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    status = response.read()
+    print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
+          .format(type(status), status, status.decode('utf-8')))
