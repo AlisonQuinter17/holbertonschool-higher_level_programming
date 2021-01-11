12a44715dc240113a71b74bcc6235adf4d4bd5d1
Alison Quintero <alisondanellaquintero@hotmail.com>
2021-01-11 13:34:44 -0500

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,3 +7,9 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     status = response.read()
     print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
           .format(type(status), status, status.decode('utf-8')))
+    """
+    Body response:
+        - type: <class 'bytes'>
+        - content: b'OK'
+        - utf8 content: OK
+    """
