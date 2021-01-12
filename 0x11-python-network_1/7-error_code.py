76e2513ebacd9ec355fe6f2638db11ae74962d36
Alison Quintero <alisondanellaquintero@hotmail.com>
2021-01-11 20:47:34 -0500

    ERROR requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     try:
         response = requests.get(sys.argv[1])
         print(response)
-    except requests.HTTPError as error:
+    except requests.exceptions.HTTPError as error:
         print("Error code:", error.code)
