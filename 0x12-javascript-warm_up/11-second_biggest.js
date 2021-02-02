dcc2333cb3053403cd80f359808c92a21f2c9d6d
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-02 18:30:11 -0500

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // The second biggest number.
 
-const array = process.argv.slice(2).sort();
+const array = process.argv.slice(2);
 
 if (array.length <= 1) {
   console.log('0');
 } else {
+    array.sort((a, b) => a - b);
   const len = array.length;
   console.log(array[len - 2]);
 }
