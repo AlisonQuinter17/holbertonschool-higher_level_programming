58c0c4908a32a5af4c8694acd0d936a5c21cb2e0
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-02 15:00:04 -0500

    Convert to integer.

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 //  prints the message if the first argument can be converted to an integer.
 
 const num = parseInt(process.argv[2]);
-if (num) {
+if (!isNaN(num)) {
   console.log('My number:', num);
 } else {
   console.log('Not a number');
