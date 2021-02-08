65d1b5d520681afeae291072815f6ac7813477dc
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-08 17:28:55 -0500

    Conditionals parameters.

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,9 @@
 module.exports = 
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-  if (w <= 0 || h <= 0) {
-    this.width = NaN;
-    this.height = NaN;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 };
