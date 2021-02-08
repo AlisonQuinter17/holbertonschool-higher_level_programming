7526b5d1cfb2a3be281e12819bb1534ced84c1fb
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-08 18:47:56 -0500

    Print rectangle.

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/nodejs
+// Print the rectangle.
+module.exports = class rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    print() {
+      for (let i = 0; i < h; i++) {
+        for (let i = 0; i < w; i++) {
+          console.log('X');
+        }
+        console.log('\n');
+      }
+    }
+  }
+};
