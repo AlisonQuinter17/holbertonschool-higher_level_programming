2e58a5080d04a87956038059acb0a60fe0128aa0
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-08 23:43:19 -0500

    Reversed array.

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/nodejs
+// Returns the reversed version of a list.
+exports.esrever = function (list) {
+  const final = 0;
+  for (let i = 0; list[i]; i++) {
+    final = i;
+  }
+  for (j = final; list[j]; j--) {
+    console.log(list[j]);
+  }
+};
