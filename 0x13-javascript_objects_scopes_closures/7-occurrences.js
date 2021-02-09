d986f263b47529423a5137ec57f336fa90b11eb9
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-08 23:27:57 -0500

    Total of ocurrences.

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/nodejs
 // function that returns the number of occurrences in a list.
 exports.nbOccurences = function (list, searchElement) {
+  let container = 0;
   for (let i = 0; list[i]; i++) {
-    let container = 0;
     if (list[i] === searchElement) {
       container += 1;
     }
