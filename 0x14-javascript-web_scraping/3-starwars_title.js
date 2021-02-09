243e3dea3502487bf6c395407cd5b4a143adf8fb
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-09 16:29:12 -0500

    Get json title of an API.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 // makes get request for SW movie id
 const request = require('request');
 
-request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  function (error, response, body) {
-    console.log(error || JSON.parse(body).title);
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
