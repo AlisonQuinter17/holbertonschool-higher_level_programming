f0ea7e4c6cf19f5b6a9ca2f3b79fb077fbfba07c
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-09 16:13:06 -0500

    Get json title of an API.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,11 +4,8 @@
   the episode number matches a given integer.
 */
 const request = require('request');
-const info = {
-  url: `https://swapi.co/api/films/${process.argv[2]}/`,
-  method: 'GET'
-};
-request(info, function (error, response, body) {
-  if (error) console.log(error);
-  else console.log(JSON.parse(body).title);
-});
+request.get(`https://swapi.co/api/films/${process.argv[2]}/`,
+  function (error, response, body) {
+    if (error) console.log(error);
+    else console.log(JSON.parse(body).title);
+  });
