4e6245e978bbea4f33fc707f673fa88da98f790a
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-09 14:35:51 -0500

    Display the status code.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 // Display the status code of a GET request.
 const request = require('request');
 const info = {
-    url: process.argv[2],
-    method: 'GET'
+  url: process.argv[2],
+  method: 'GET'
 };
 request(info, (err, response) => {
-    if (err) throw err;
-    console.log('code:', response.statusCode);
+  if (err) throw err;
+  console.log('code:', response.statusCode);
 });
