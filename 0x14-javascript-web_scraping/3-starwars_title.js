0b552ab8e99bb470c59fc03cd36f05ab99945117
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-09 15:19:38 -0500

    Get json title of an API.

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/nodejs
+/* 
+- Prints the title of a Star Wars movie where
+  the episode number matches a given integer.
+*/
+const request = require('request');
+const info = {
+  url: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  method: 'GET'
+};
+request(info, (err, body) => {
+  if (err) throw err;
+  console.log(JSON.parse(body).title);
+});
