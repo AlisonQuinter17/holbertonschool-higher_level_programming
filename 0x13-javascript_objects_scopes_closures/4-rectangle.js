781b778d8580a02e548b64006b8c670faa792531
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-08 21:30:19 -0500

    Print, rottate and double of rectangle.

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/nodejs
+// Print, rotate and multiplies the rectangle.
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      for (; this.height; this.height--) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+    rotate () {
+      temp = this.width;
+      this.width = this.height;
+      this.width = temp;
+    }
+    double() {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  };
+  
