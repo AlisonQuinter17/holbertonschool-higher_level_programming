f1c2bd7b504e50565a479a51428752697ebccdb4
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-09 19:06:49 -0500

    Number of tasks.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/nodejs
+// Computes the number of tasks completed by user id.
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) throw error;
+  else {
+    const d = {};
+    let total = 0;
+    for (const task of JSON.parse(body)) {
+      if (d[task.userId] (task.completed) ? d[task.userId]++ : d[task.userId] = 1);
+   }
+   console.log(d);
+  }
+});
